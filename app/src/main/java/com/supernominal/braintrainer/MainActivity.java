4d06f9f03a50ac418a6e0cab45dfb458376b28ae
Harry Kim <hkusa121@gmail.com>
2018-02-20 22:17:31 -0800

    Minor change to setup Android Studio Github integration

--- app/src/main/java/com/supernominal/braintrainer/MainActivity.java
+++ app/src/main/java/com/supernominal/braintrainer/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     ConstraintLayout gameConstraintLayout;
     TextView timerTextView;
     TextView finalScoreTextView;
+    
 
     public void startGame(View view ) {
 
